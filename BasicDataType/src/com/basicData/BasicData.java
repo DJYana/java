f0bfb333b8f6fbdf094f131ca0b81230632f8c8a
DJ Yana <1793792599@qq.com>
2019-05-01 13:37:20 +0800

    [add] java 新增 八种基本数据类型

---
+++ BasicDataType/src/com/basicData/BasicData.java
@@ -0,0 +1,94 @@
+package com.basicData;/**
+ * Create By DJ Yana ON  2019/5/1 13:32
+ */
+
+/**
+ *@Author: DJ Yana
+ *@Description: java 八种基本数据类型
+ *@Date: Create In  2019/5/1 13:32
+ *@Modify By:
+ */
+public class BasicData {
+
+
+    private byte  aByte;
+
+
+    private short aShort;
+
+    private int anInt;
+
+    private long aLong;
+
+    private char aChar;
+
+    private double aDouble;
+
+    private float aFloat;
+
+    private  boolean aBoolean;
+
+    public byte getaByte() {
+        return aByte;
+    }
+
+    public void setaByte(byte aByte) {
+        this.aByte = aByte;
+    }
+
+    public short getaShort() {
+        return aShort;
+    }
+
+    public void setaShort(short aShort) {
+        this.aShort = aShort;
+    }
+
+    public int getAnInt() {
+        return anInt;
+    }
+
+    public void setAnInt(int anInt) {
+        this.anInt = anInt;
+    }
+
+    public long getaLong() {
+        return aLong;
+    }
+
+    public void setaLong(long aLong) {
+        this.aLong = aLong;
+    }
+
+    public char getaChar() {
+        return aChar;
+    }
+
+    public void setaChar(char aChar) {
+        this.aChar = aChar;
+    }
+
+    public double getaDouble() {
+        return aDouble;
+    }
+
+    public void setaDouble(double aDouble) {
+        this.aDouble = aDouble;
+    }
+
+    public float getaFloat() {
+        return aFloat;
+    }
+
+    public void setaFloat(float aFloat) {
+        this.aFloat = aFloat;
+    }
+
+    public boolean isaBoolean() {
+        return aBoolean;
+    }
+
+    public void setaBoolean(boolean aBoolean) {
+        this.aBoolean = aBoolean;
+    }
+}
